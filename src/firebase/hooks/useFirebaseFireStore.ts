e3589c579c5c11f4ebee2f8483623f4c449a2b96
RyumenSukuna14 <awaisazeemsahi786@gmail.com>
2026-07-07 15:52:28 +0500

    created custom hook for firebase firestore to set, get, and update users in firestore

---
+++ src/firebase/hooks/useFirebaseFireStore.ts
@@ -0,0 +1,38 @@
+import {db} from '../firebase'
+import {doc, getDoc, setDoc, serverTimestamp, updateDoc} from 'firebase/firestore';
+import {type User} from 'firebase/auth';
+import { type AppUser } from '../../types/index';
+// import { useState } from 'react';
+
+function useFirebaseFireStore(){
+    // const [currentUser, setCurrentUser] = useState<AppUser | null>(null);
+    const setUserData = async (user: User) => {
+        if(!user) return;
+        await setDoc(doc(db, 'users', user.uid), {
+                displayName: user.displayName ?? '',
+                username: null,
+                photoURL: user.photoURL ?? '',
+                email: user.email ?? '',
+                bio: '',
+                followerCount: 0,
+                createdAt: serverTimestamp(),
+        })
+    }
+    const getUserData = async (user: User): Promise<AppUser | null> => {
+        if(!user) return null;
+        const docSnap = await getDoc(doc(db, 'users', user.uid));
+        if(docSnap.exists()){
+            return docSnap.data() as AppUser;
+        } else {
+            console.log('this user doesnt exist!')
+            return null;
+        }
+    }
+    const updateUserData = async (user: User, updates: Partial<AppUser>) => {
+        if(!user) return;
+        await updateDoc(doc(db, 'users', user.uid), updates)
+    }
+    return { setUserData, getUserData, updateUserData };
+}
+
+export default useFirebaseFireStore;
