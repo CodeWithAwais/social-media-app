e35aded1817f85a89b1afca8a1cfa509feb88867
RyumenSukuna14 <awaisazeemsahi786@gmail.com>
2026-07-23 15:06:19 +0500

    created fireStoreUser ts file dedicated for user related func of firestore

---
+++ src/firebase/hooks/useFireStoreUser.ts
@@ -0,0 +1,39 @@
+import {db} from '../firebase'
+import {doc, getDoc, setDoc, serverTimestamp, updateDoc} from 'firebase/firestore';
+import {type User} from 'firebase/auth';
+import { type UserProfile } from '../../types/index';
+
+function useFirebaseFireStore(){
+    const setUserData = async (user: User) => {
+        if(!user) return;
+        await setDoc(doc(db, 'users', user.uid), {
+                userId: user.uid,
+                displayName: user.displayName ?? '',
+                username: null,
+                photoURL: user.photoURL ?? '',
+                email: user.email ?? '',
+                bio: '',
+                followerCount: 0,
+                createdAt: serverTimestamp(),
+        })
+    }
+    const getUserData = async (user: User): Promise<UserProfile | null> => {
+        if(!user) return null;
+        const docSnap = await getDoc(doc(db, 'users', user.uid));
+        if(docSnap.exists()){
+            return docSnap.data() as UserProfile;
+        } else {
+            console.log('this user doesnt exist!')
+            return null;
+        }
+    }
+    const updateUserData = async (user: User, updates: Partial<UserProfile>) => {
+        if(!user) return;
+        await updateDoc(doc(db, 'users', user.uid), updates)
+    }
+
+    
+    return { setUserData, getUserData, updateUserData };
+}
+
+export default useFirebaseFireStore;
